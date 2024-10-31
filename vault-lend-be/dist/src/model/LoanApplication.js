46ba76fa7a7f5aeca048436102fe6b859442aff8
marciogh <marcio.gh@gmail.com>
2024-10-31 11:43:51 -0300

    removing dist

--- vault-lend-be/dist/src/model/LoanApplication.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoanPurpose = void 0;
-var LoanPurpose;
-(function (LoanPurpose) {
-    LoanPurpose[LoanPurpose["Vehicle"] = 0] = "Vehicle";
-    LoanPurpose[LoanPurpose["HomeImprovement"] = 1] = "HomeImprovement";
-})(LoanPurpose || (exports.LoanPurpose = LoanPurpose = {}));
-//# sourceMappingURL=LoanApplication.js.map
